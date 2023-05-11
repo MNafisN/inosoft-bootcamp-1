9f68cb57154c21bf7188cd0deb61910d3007b7ac
MNafisN <naf1s.n4ufally@gmail.com>
2023-05-11 13:38:40 +0700

    Implementasi Autentikasi dan Autorisasi JWT

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,15 +8,18 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+use Jenssegers\Mongodb\Auth\User as AuthenticatableMongo;
 use Illuminate\Auth\Authenticatable as AuthenticableTrait;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 
-class User extends Eloquent implements AuthenticatableContract
+class User extends AuthenticatableMongo implements JWTSubject
 {
-    use AuthenticableTrait, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     protected $connection = 'mongodb';
+    protected $collection = 'users';
 
     /**
      * The attributes that are mass assignable.
@@ -47,4 +50,14 @@ class User extends Eloquent implements AuthenticatableContract
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getJWTCustomClaims()
+    {
+        return[];
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
 }
